e374e2d67364daf136a11b6aecb333eb46a6eabc
Nicola Hastings <nicolahastings@gmail.com>
2020-07-22 10:14:26 -0400

    NH | Announcement functionality

--- coh-general.js
+++ coh-general.js
@@ -1,7 +1,15 @@
 $(function () {
     handleSearch();
+    handleAnnouncement();
 });
 
 function handleSearch() {
     $('<button type="button" class="search-btn-top" />').insertBefore('.HLWelcome');
+}
+
+function handleAnnouncement() {
+    $('.announcement-bar').append('<button type="button" onclick="closeAnnouncement();"><i class="fal fa-times"></i></button>');
+}
+function closeAnnouncement() {
+    $('.announcement-bar').hide();
 }
