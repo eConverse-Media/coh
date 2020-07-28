8a04b89181ce5dbcba1bd49728fb50b54645a54f
Nicola Hastings <nicolahastings@gmail.com>
2020-07-28 14:01:05 -0400

    NH | testimonials

--- coh-slick.js
+++ coh-slick.js
@@ -7,4 +7,13 @@ $(function () {
         fade: true,
         autoplay: true
     });
+    $('.testimonial').wrapAll('<div class="testimonial-slider slick-dotted" />');
+    $('.testimonial-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        autoplay: false,
+        prevArrow: '<button type="button" class="prev-arrow slick-arrow"><i class="ft ft-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="next-arrow slick-arrow"><i class="ft ft-chevron-right"></i></button>'
+    });
 });
